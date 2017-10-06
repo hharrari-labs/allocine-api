e573b0108e6534195d0aa290dcaae13f1645d0fa
Hakim <kim.k866@gmail.com>
2017-10-06 19:05:54 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+<?php
+
+require_once(__DIR__.'/allocine.class.php');
+
+define('ALLOCINE_PARTNER_KEY', '100043982026');
+define('ALLOCINE_SECRET_KEY', '29d185d98c984a359e6e6f26a0474269');
+
+$allocine = new Allocine(ALLOCINE_PARTNER_KEY, ALLOCINE_SECRET_KEY);
+$user_search = htmlspecialchars($_GET["movie"]);
+$result = $allocine->search($user_search);
+
+echo $result;
+?>
+    </body>
+</html>
